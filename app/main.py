572835759081b00802e4b00e08dfcb0e6fbcd006
Sneha Nagargoje <snehanagargoje@gmail.com>
2026-01-26 16:49:11 +0530

    final backend structure

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(title="AI Counsellor API")
 
 @app.get("/")
 def root():
     return {"status": "AI Counsellor backend running"}
+
+class CounselRequest(BaseModel):
+    message: str
+
+@app.post("/counsel")
+def counsel_user(data: CounselRequest):
+    return {
+        "user_message": data.message,
+        "counsellor_reply": "I understand how you feel. I'm here to help you."
+    }
